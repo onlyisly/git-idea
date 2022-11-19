da1ebba38d546d616198e4f9ee85b2fcba2581d7
stitch <stitch@163.com>
2022-11-19 16:34:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * projectName: ssm
+ *
+ * @author: 白应南
+ * time: 2022/11/19 16:31 周六
+ * description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
